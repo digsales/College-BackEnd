b3d73f5324577d6aab414a88bec1ac6629f21ace
_digsales <diogodobu@gmail.com>
2022-10-27 10:21:31 -0300

    Update SemestreValidator #1

--- app/Validators/SemestreValidator.ts
+++ app/Validators/SemestreValidator.ts
@@ -5,12 +5,10 @@ export default class SemestreValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    nome: schema.string([
-      rules.maxLength(30),
-      rules.unique({ table: "semestres", column: "nome" }),
-    ]),
+    nome: schema.string([rules.maxLength(30)]),
     capacidade: schema.date(),
     tipo: schema.date(),
   });
+
   public messages: CustomMessages = {};
 }
